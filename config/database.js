8e1b1fe4326b1f94058f413703108aed0fafdaec
KhachikHarutyunyan <khachik789@gmail.com>
2018-05-29 11:26:48 +0400

    linked db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  // database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://testuser:123456@ds139690.mlab.com:39690/meanauthapp',
   secret: 'yoursecret'
 }
 
